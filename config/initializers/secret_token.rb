19bbb3936775692e7faa57303596511f462d0f0c
Abhishek Kumar <abhishek.kumar@icicletech.com>
2014-04-12 23:36:23 +0530

    Basic rails app with default setting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MvitAToZ::Application.config.secret_token = 'e2c11f930d76744e9996cc3c7de1ba48f5abd3e9b91bbe07f860bbb4a8b4237edb23ffeb357f656709e70ffd8a4ae49ca55f91a40847668ca4f99e699f837ecb'
